bdd369bf975e1e13998e427e8fc1eecbea6c4322
michael-citi <mciti5211@gmail.com>
2019-02-26 15:32:34 -0800

    Updates

--- app/src/main/java/com/michaelciti/c196project/MainActivity.java
+++ app/src/main/java/com/michaelciti/c196project/MainActivity.java
@@ -1,10 +1,8 @@
 package com.michaelciti.c196project;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
     }
 

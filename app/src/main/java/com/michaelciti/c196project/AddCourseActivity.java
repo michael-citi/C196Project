6272d799735c5cc17684f9c9a935fd678cac5e60
michael-citi <mciti5211@gmail.com>
2019-03-29 13:52:33 -0700

    Restructuring

---
+++ app/src/main/java/com/michaelciti/c196project/AddCourseActivity.java
@@ -0,0 +1,20 @@
+package com.michaelciti.c196project;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AddCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}

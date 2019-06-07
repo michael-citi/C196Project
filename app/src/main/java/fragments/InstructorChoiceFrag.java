f49f8ee8f56aeaa69078e2a1a68778fe211270e1
Michael <mciti5211@gmail.com>
2019-06-06 17:51:05 -0700

    Updates

--- app/src/main/java/fragments/InstructorChoiceFrag.java
+++ app/src/main/java/fragments/InstructorChoiceFrag.java
@@ -9,21 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.michaelciti.c196project.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link InstructorChoiceFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link InstructorChoiceFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InstructorChoiceFrag extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -33,15 +24,6 @@ public class InstructorChoiceFrag extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InstructorChoiceFrag.
-     */
-    // TODO: Rename and change types and number of parameters
     public static InstructorChoiceFrag newInstance(String param1, String param2) {
         InstructorChoiceFrag fragment = new InstructorChoiceFrag();
         Bundle args = new Bundle();
--- app/src/main/java/fragments/ObjectiveChoiceFrag.java
+++ app/src/main/java/fragments/ObjectiveChoiceFrag.java
@@ -10,21 +10,12 @@ import android.view.ViewGroup;
 
 import com.michaelciti.c196project.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ObjectiveChoiceFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ObjectiveChoiceFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ObjectiveChoiceFrag extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -34,15 +25,6 @@ public class ObjectiveChoiceFrag extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ObjectiveChoiceFrag.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ObjectiveChoiceFrag newInstance(String param1, String param2) {
         ObjectiveChoiceFrag fragment = new ObjectiveChoiceFrag();
         Bundle args = new Bundle();
